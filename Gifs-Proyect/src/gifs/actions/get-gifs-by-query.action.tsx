8c97ba074b1c8d708245238651607a2c0f5da8c8
Sergio Vargas <svargas.ext.uber@gmail.com>
2025-11-10 21:46:55 -0500

    first part finished

---
+++ Gifs-Proyect/src/gifs/actions/get-gifs-by-query.action.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import type {GiphyResponse} from '../interfaces/giphy.response'
+import {Gif} from '../interfaces/gif.interface'
+
+export const  getGifByQuery = async (query : string): Promise<Gif[]> => { 
+    
+const response = await axios.get<GiphyResponse>('https://api.giphy.com/v1/gifs/search?', {
+      params: {
+        q: query,
+        limit: 6,
+        lang: 'es',
+        api_key: 'VITE_GIPHY_API_KEY='
+      }
+    })
+
+
+    return response.data.data.map((gif) => ({
+      id: gif.id,
+      title: gif.title,
+      url: gif.images.original.url,
+      width: Number(gif.images.original.width),
+      height: Number(gif.images.original.height)
+    })) 
+  }
+
+  
